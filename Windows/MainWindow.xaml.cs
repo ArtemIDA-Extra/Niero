20cd1bef27d11522b7eb184bafcc10281a89dbdf
Artem_IDA <artem-soroka-2013@ukr.net>
2020-08-24 20:57:39 +0300

    Intimidation commit

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,20 +11,40 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Niero
+namespace Niero.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        LoadingWindow loadingWindow;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.WindowState = WindowState.Minimized;
+
+            loadingWindow = new LoadingWindow();
+            loadingWindow.Show();
+
+            this.Loaded += MainWindow_Loaded;
+
+            loadingWindow.Closed += LoadingWindow_Closed;
+        }
+
+        public async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Factory.StartNew(()=> { Thread.Sleep(5000);                     //Жопой чую, это МЕГА костылина, и есть куча других способов создать таймер 
+            });                                                                        //кдасс таймер у меня не сконал (не смог поставить коллбеком Close()), а из других потоков не удается 
+            loadingWindow.Close();                                                     //достучатся до загрузочного окна. В общем, пока так оставлю xDDDD
+        }
+
+        public void LoadingWindow_Closed(object sender, EventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
         }
     }
 }
-
