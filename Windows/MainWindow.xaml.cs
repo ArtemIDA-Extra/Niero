07df2c634cf5096d2401cfe72e0d7d6cfc03712e
Artem_IDA <artem-soroka-2013@ukr.net>
2020-09-21 23:20:05 +0300

    Small fixing.

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
@@ -18,14 +19,38 @@ namespace Niero.Windows
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         LoadingWindow loadingWindow;
         Button maxSizeButton, minSizeButton, closeWindowButton;
 
+        private int OuterWindowMargin = 10;
+        private int ResizeBorderSize = 11;
+
+        public Thickness OuterMarginSizeThickness 
+        {
+            get 
+            { 
+                return new Thickness(OuterWindowMargin); 
+            } 
+        }
+        public Thickness ResizeBorderThickness
+        {
+            get
+            {
+                return new Thickness(ResizeBorderSize);
+            }
+        }
+        public int CaptionHeightProperty 
+        { 
+            get; 
+            private set; 
+        } = 24;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
             this.WindowState = WindowState.Minimized;
 
             //Init loading window 
@@ -38,12 +63,45 @@ namespace Niero.Windows
             minSizeButton = (Button)GetTemplateChild("MinSizeButton");
             closeWindowButton = (Button)GetTemplateChild("CloseWindowButton");
 
-            //Commands time!!!
+            //Commands time!
             CommandsInit();
 
             //Window events binding
+            this.StateChanged += MainWindow_StateChanged;
             this.Loaded += MainWindow_Loaded;
             loadingWindow.Closed += LoadingWindow_Closed;
+        }
+
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            switch (this.WindowState) 
+            {
+                case WindowState.Normal :
+                    OuterWindowMargin = 10;
+                    ResizeBorderSize = 11;
+                    CaptionHeightProperty = (OuterWindowMargin + 25 - ResizeBorderSize);
+                    OnPropertyChanged(nameof(ResizeBorderThickness));
+                    OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                    OnPropertyChanged(nameof(CaptionHeightProperty));
+                    break;
+                case WindowState.Maximized :
+                    this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+                    OuterWindowMargin = 7;
+                    ResizeBorderSize = 0;
+                    CaptionHeightProperty = (OuterWindowMargin + 25 - ResizeBorderSize);
+                    OnPropertyChanged(nameof(ResizeBorderThickness));
+                    OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                    OnPropertyChanged(nameof(CaptionHeightProperty));
+                    break;
+                case WindowState.Minimized :
+                    OuterWindowMargin = 10;
+                    ResizeBorderSize = 11;
+                    CaptionHeightProperty = (OuterWindowMargin + 25 - ResizeBorderSize);
+                    OnPropertyChanged(nameof(ResizeBorderThickness));
+                    OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                    OnPropertyChanged(nameof(CaptionHeightProperty));
+                    break;
+            }
         }
 
         private void CommandsInit()
@@ -92,17 +150,22 @@ namespace Niero.Windows
             Close();
         }
 
-        public async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             await Task.Factory.StartNew(()=> { Thread.Sleep(3000);                     //Жопой чую, это МЕГА костылина, и есть куча других способов создать таймер 
             });                                                                        //кдасс таймер у меня не сконал (не смог поставить коллбеком Close()), а из других потоков не удается 
             loadingWindow.Close();                                                     //достучатся до загрузочного окна. В общем, пока так оставлю xDDDD
         }
 
-        public void LoadingWindow_Closed(object sender, EventArgs e)
+        private void LoadingWindow_Closed(object sender, EventArgs e)
         {
             this.WindowState = WindowState.Normal;
         }
 
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
